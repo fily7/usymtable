06e4a33ca8d8e4d8aa6ce9960dd9f85d8fea0b51
developer <developer@example.com>
2024-09-13 10:43:44 +0000

    Aga

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const (
+	LINES   = 20
+	SYMBOLS = 15
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	start := 0xf000
+	end := 0xffff
+	for {
+		fmt.Printf("For print next %d lines press n to quit press q\n", LINES)
+		text, _, err := reader.ReadRune()
+		if err != nil {
+			panic(err)
+		}
+		if text == 'q' {
+			os.Exit(0)
+		}
+		for j := 0; j < LINES; j++ {
+			for i := 0; i < SYMBOLS; i++ {
+				if start+i > end {
+					break
+				}
+				fmt.Printf("   %c   ", start+i)
+			}
+			fmt.Println(" ")
+			for i := 0; i < SYMBOLS; i++ {
+				if start+i > end {
+					os.Exit(0)
+				}
+				fmt.Printf(" u%04x ", start+i)
+			}
+			fmt.Println(" ")
+			start += SYMBOLS
+		}
+	}
+}
